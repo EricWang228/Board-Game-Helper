019e339ad28d2ee118eba98ea98a5cd0282e690e
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2022-11-29 14:45:12 -0500

    Update BoardGame.h

--- BoardGameOBJ/BoardGame.h
+++ BoardGameOBJ/BoardGame.h
@@ -24,3 +24,55 @@ class BoardGame
     // print object
     void print_details();
 };
+
+BoardGame::BoardGame()
+{
+    name = "";
+    year = 0;
+    min_player = 0;
+    max_players = 0;
+    age = 0;
+}
+
+BoardGame::BoardGame(std::string name, unsigned int year, unsigned int min_players, unsigned int max_players, unsigned int age)
+{
+    this->name = name;
+    this->year = year;
+    this->min_player = min_player;
+    this->max_players = max_players;
+    this->age = age;
+}
+
+std::string BoardGame::get_name()
+{
+    return name;
+}
+
+unsigned int BoardGame::get_year()
+{
+    return year;
+}
+
+unsigned int BoardGame::get_min_players()
+{
+    return min_player;
+}
+
+unsigned int BoardGame::get_max_players()
+{
+    return max_players;
+}
+
+unsigned int BoardGame::get_age()
+{
+    return age;
+}
+
+void BoardGame::print_details()
+{
+    std::cout << "Board Game Name: " << name << std::endl;
+    std::cout << "Year Published: " << year << std::endl;
+    std::cout << "Min Players: " << min_player << std::endl;
+    std::cout << "Max Players: " << max_players << std::endl;
+    std::cout << "Min Age: " << age << std::endl;
+}
