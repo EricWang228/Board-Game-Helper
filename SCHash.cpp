9e56df5fda06ad4881397cc7436e280d473aa343
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2022-12-06 15:42:27 -0500

    Update SCHash.cpp

--- SCHash.cpp
+++ SCHash.cpp
@@ -11,17 +11,20 @@ class SCHash{
             string key;
             BoardGame value;
         };
+        // Variables
+        int cap = 16;
+        int table_size = 0;
+        double l_factor = 0.75;
+        // Container
+        vector<list<item*>> hash_table; 
         item* create_item(string& key, BoardGame& value);
         unsigned long hash_function(string& key); 
-        // Each index maps to a list for SC resolution
-        vector<list<item*>> hash_table; 
-        int cap = 16;
-        int b_size = 0;
-        double l_factor = 0.75;
-        // For both updated_table inserts and current hash_table inserts
+        BoardGame empty;
+        // Helper for insert, supports both regular and updated inserts
         void private_insert(vector<list<item*>>& map, string key, BoardGame& value); 
         // Resize table for Load Factor
         vector<list<item*>> update_table(); 
+        // For default returns
     public:
         // Default Constructor
         SCHash();
@@ -29,13 +32,14 @@ class SCHash{
         ~SCHash();
         // Getters
         int capacity();
-        int bucket_size();
+        int size();
         double load_factor();
         // Insertion and deletion 
         void insert(string key, BoardGame& value);
         void remove(string key);
         // Search
         vector<BoardGame> find_all(string target);
+        BoardGame find(string target);
 }; 
 
 typename SCHash::item* SCHash::create_item(string& key, BoardGame& value){
@@ -56,7 +60,52 @@ unsigned long SCHash::hash_function(string& key){
     return hash;
 }
 
+void SCHash::private_insert(vector<list<item*>>& map, string key, BoardGame& value){
+    // Gets hashed index
+    unsigned long index = hash_function(key);
+    table_size++;
+    // Finds the Linked list at the index
+    auto& Bucket = map[index];
+    bool keyExists = false;
+    // Runs through the Linked List at the Bucket index, checking for existing keys
+    for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
+        if((*iter)->key == key){
+            keyExists = true;
+            table_size--;
+            (*iter)->value = value;
+            break;
+        }
+    }
+    // Otherwise append to end of Linked List (Seperate Chaining)
+    if(!keyExists){
+        Bucket.emplace_back(create_item(key, value));
+    }
+}
+
+vector<list<SCHash::item*>> SCHash::update_table(){
+    cap = 2*cap;
+    // Make new table with double the size
+    vector<list<item*>> new_table;
+    for(int i = 0; i < cap; i++){
+        list<item*> temp;
+        new_table.push_back(temp);
+    }
+    // Rehashes all items in the old table into the new table
+    for(int j = 0; j < hash_table.size(); j++){
+        auto& Bucket = hash_table[j];
+        for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
+            private_insert(new_table, (*iter)->key, (*iter)->value);
+            table_size--;
+            // Free memory
+            delete (*iter);
+            iter = --Bucket.erase(iter);
+        }
+    }
+    return new_table;
+}
+
 SCHash::SCHash(){
+    // Allocate space for Map
     for(int i = 0; i < cap; i++){
         list<item*> temp;
         hash_table.push_back(temp);
@@ -66,68 +115,29 @@ SCHash::SCHash(){
 SCHash::~SCHash(){
     // Loops through all indicies and deletes all item* in the list structure
     for(int i = 0; i < cap; i++){
-        auto& block = hash_table[i];
-        for (auto iter = block.begin(); iter != block.end(); iter++){
+        auto& Bucket = hash_table[i];
+        for (auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
             delete(*iter);
-            iter = --block.erase(iter);
+            iter = --Bucket.erase(iter);
         }
     }
 }
 
-// Getters
 int SCHash::capacity(){
     return cap;
 }
 
-int SCHash::bucket_size(){
-    return b_size;
+int SCHash::size(){
+    return table_size;
 }
 
 double SCHash::load_factor(){
     return l_factor;
 }
 
-void SCHash::private_insert(vector<list<item*>>& map, string key, BoardGame& value){
-    unsigned long index = hash_function(key);
-    b_size++;
-    auto& block = map[index];
-    bool keyExists = false;
-    for(auto iter = block.begin(); iter != block.end(); iter++){
-        if((*iter)->key == key){
-            keyExists = true;
-            b_size--;
-            (*iter)->value = value;
-            break;
-        }
-    }
-    if(!keyExists){
-        block.emplace_back(create_item(key, value));
-    }
-}
-
-vector<list<SCHash::item*>> SCHash::update_table(){
-    // Double capacity
-    cap = 2*cap;
-    // Make new table with double the size
-    vector<list<item*>> new_table;
-    for(int i = 0; i < cap; i++){
-        list<item*> temp;
-        new_table.push_back(temp);
-    }
-    for(int j = 0; j < hash_table.size(); j++){
-        auto& block = hash_table[j];
-        for(auto iter = block.begin(); iter != block.end(); iter++){
-            private_insert(new_table, (*iter)->key, (*iter)->value);
-            b_size--;
-            delete (*iter);
-            iter = --block.erase(iter);
-        }
-    }
-    return new_table;
-}
-
 void SCHash::insert(string key, BoardGame& value){
-    if(((double)b_size/cap) >= l_factor){ // Load Factor reached double size of hashmap
+    // Determines if table needs to be refactored to new size
+    if(((double)table_size/cap) >= l_factor){ 
         hash_table = update_table();
         private_insert(hash_table, key, value);
     }
@@ -138,25 +148,51 @@ void SCHash::insert(string key, BoardGame& value){
 
 void SCHash::remove(string key){
     unsigned long index = hash_function(key);
-    auto& block = hash_table[index];
-    for(auto iter = block.begin(); iter != block.end(); iter++){
+    auto& Bucket = hash_table[index];
+    for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
         if((*iter)->key == key){
-            b_size--;
-            iter = block.erase(iter);
+            table_size--;
+            iter = Bucket.erase(iter);
             break;
         }
     }
 }
 
 vector<BoardGame> SCHash::find_all(string target){
-    vector<BoardGame> targets;
+    vector<BoardGame> vals;
+    // Loops though all 
     for(int i = 0; i < hash_table.size(); i++){
-        auto& block = hash_table[i];
-        for(auto iter = block.begin(); iter != block.end(); iter++){
+        auto& Bucket = hash_table[i];
+        for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
             if((*iter)->key.find(target) != string::npos){
-                targets.push_back((*iter)->value);
+                vals.push_back((*iter)->value);
             }
         }
     }
-    return targets;
+    return vals;
+}
+
+BoardGame SCHash::find(string target){
+    unsigned long index = hash_function(target);
+    auto& Bucket = hash_table[index];
+    // Searches in potential bucket for target first
+    for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
+        if((*iter)->key.find(target) != string::npos){
+                return (*iter)->value;
+            } 
+    }
+    // Target is not in potential bucket
+    for(int i = 0; i < hash_table.size(); i++){
+        // Bucket is Searched already
+        if(i = index)
+            continue;
+        Bucket = hash_table[i];
+        for(auto iter = Bucket.begin(); iter != Bucket.end(); iter++){
+            if((*iter)->key.find(target) != string::npos){
+                return (*iter)->value;
+            }
+        }
+    }
+    // Nothing is found
+    return empty;
 }
